6f423a5486ed165f8f483182df23bf3b576f1751
Cindy Wang <ciwang@stanford.edu>
2015-02-21 21:20:19 -0800

    changed indent

--- src/main/java/com/atomiton/smartparking/GroupSmartParking.java
+++ src/main/java/com/atomiton/smartparking/GroupSmartParking.java
@@ -1,9 +1,8 @@
 package com.atomiton.smartparking;
 
 public class GroupSmartParking {
-public static void main(String[] args) {
-	System.out.printf("Hello group");
-
-}
+	public static void main(String[] args) {
+		System.out.printf("Hello group");
 	
+	}	
 }
