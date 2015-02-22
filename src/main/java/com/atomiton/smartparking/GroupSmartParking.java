98174a9817b9ef7188a0fe21d1a0dadd5c5f52f0
Daniel Lai <lai.daniel.k@gmail.com>
2015-02-21 23:41:35 -0800

    event listener

--- src/main/java/com/atomiton/smartparking/GroupSmartParking.java
+++ src/main/java/com/atomiton/smartparking/GroupSmartParking.java
@@ -1,14 +1,16 @@
 package com.atomiton.smartparking;
 
 public class GroupSmartParking {
-<<<<<<< HEAD
+//<<<<<<< HEAD
 	public static void main(String[] args) {
 		System.out.printf("Hello group");
-=======
-public static void main(String[] args) {
-	System.out.printf("Hello group");
+	}
 }
->>>>>>> aed5b7923a7b1dc606c0cf365b4bb2cb8b15fbb3
-	
-	}	
-}
+//=======
+//public static void main(String[] args) {
+//	System.out.printf("Hello group");
+//}
+//>>>>>>> aed5b7923a7b1dc606c0cf365b4bb2cb8b15fbb3
+//	
+//	}	
+//}
